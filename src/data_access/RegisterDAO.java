4dc3ef4b8ead75d75d7ca1797f60f90fb0755542
Yale <y.wang217@gmail.com>
2019-11-27 17:21:43 -0500

    add register update

--- src/data_access/RegisterDAO.java
+++ src/data_access/RegisterDAO.java
@@ -1,10 +1,35 @@
 package data_access;
 
-public class RegisterDAO implements ExampleDAO{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	@Override
-	public String retrieve(String query) {
+public class RegisterDAO extends ExampleDaoImpl{
+	
+	
+	public int update(String query) {
+		int res = 0;
+		try {
+			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			Statement stmt = conn.createStatement();
+
+			res = stmt.executeUpdate(query);
+			//res==1 is success;
+			
+			printUpdateStatus(res, query);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return res;
+	}
+	public static void main(String[] argv) {
+		RegisterDAO e = new RegisterDAO();
+		e.update("INSERT INTO  `Customers` (`username`, `password`, `Cust_name`) VALUES ('2', 'password', 'temp');");
 		
 	}
-
 }
